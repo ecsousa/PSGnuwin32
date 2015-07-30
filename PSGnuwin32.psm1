e227e5f26335c1fe7ade32b94dd837882a47d935
Eduardo Antunes C. de Sousa <ecsousa@gmail.com>
2015-07-30 18:20:44 -0300

    Initial commit, extracted from PSUtils

---
+++ PSGnuwin32.psm1
@@ -0,0 +1,319 @@
+Set-Alias g-cat (Join-Path $PSScriptRoot 'bin\cat.exe')
+Set-Alias g-cp (Join-Path $PSScriptRoot 'bin\cp.exe')
+Set-Alias g-dir (Join-Path $PSScriptRoot 'bin\dir.exe')
+Set-Alias g-echo (Join-Path $PSScriptRoot 'bin\echo.exe')
+Set-Alias g-kill (Join-Path $PSScriptRoot 'bin\kill.exe')
+Set-Alias g-ls (Join-Path $PSScriptRoot 'bin\ls.exe')
+Set-Alias g-mkdir (Join-Path $PSScriptRoot 'bin\mkdir.exe')
+Set-Alias g-mv (Join-Path $PSScriptRoot 'bin\mv.exe')
+Set-Alias g-pwd (Join-Path $PSScriptRoot 'bin\pwd.exe')
+Set-Alias g-rm (Join-Path $PSScriptRoot 'bin\rm.exe')
+Set-Alias g-rmdir (Join-Path $PSScriptRoot 'bin\rmdir.exe')
+Set-Alias g-sleep (Join-Path $PSScriptRoot 'bin\sleep.exe')
+Set-Alias g-sort (Join-Path $PSScriptRoot 'bin\sort.exe')
+Set-Alias g-tee (Join-Path $PSScriptRoot 'bin\tee.exe')
+Set-Alias g-wget (Join-Path $PSScriptRoot 'bin\wget.exe')
+
+Set-Alias awk (Join-Path $PSScriptRoot 'bin\awk.exe')
+Set-Alias basename (Join-Path $PSScriptRoot 'bin\basename.exe')
+Set-Alias bunzip2 (Join-Path $PSScriptRoot 'bin\bunzip2.exe')
+Set-Alias bzcat (Join-Path $PSScriptRoot 'bin\bzcat.exe')
+Set-Alias bzcmp (Join-Path $PSScriptRoot 'bin\bzcmp.exe')
+Set-Alias bzdiff (Join-Path $PSScriptRoot 'bin\bzdiff.exe')
+Set-Alias bzegrep (Join-Path $PSScriptRoot 'bin\bzegrep.exe')
+Set-Alias bzfgrep (Join-Path $PSScriptRoot 'bin\bzfgrep.exe')
+Set-Alias bzgrep (Join-Path $PSScriptRoot 'bin\bzgrep.exe')
+Set-Alias bzip2 (Join-Path $PSScriptRoot 'bin\bzip2.exe')
+Set-Alias bzip2 (Join-Path $PSScriptRoot 'bin\bzip2.exe')
+Set-Alias bzip2recover (Join-Path $PSScriptRoot 'bin\bzip2recover.exe')
+Set-Alias bzless (Join-Path $PSScriptRoot 'bin\bzless.exe')
+Set-Alias bzmore (Join-Path $PSScriptRoot 'bin\bzmore.exe')
+Set-Alias chgrp (Join-Path $PSScriptRoot 'bin\chgrp.exe')
+Set-Alias chmod (Join-Path $PSScriptRoot 'bin\chmod.exe')
+Set-Alias chown (Join-Path $PSScriptRoot 'bin\chown.exe')
+Set-Alias chroot (Join-Path $PSScriptRoot 'bin\chroot.exe')
+Set-Alias cksum (Join-Path $PSScriptRoot 'bin\cksum.exe')
+Set-Alias comm (Join-Path $PSScriptRoot 'bin\comm.exe')
+Set-Alias csplit (Join-Path $PSScriptRoot 'bin\csplit.exe')
+Set-Alias cut (Join-Path $PSScriptRoot 'bin\cut.exe')
+Set-Alias date (Join-Path $PSScriptRoot 'bin\date.exe')
+Set-Alias dd (Join-Path $PSScriptRoot 'bin\dd.exe')
+Set-Alias df (Join-Path $PSScriptRoot 'bin\df.exe')
+Set-Alias dircolors (Join-Path $PSScriptRoot 'bin\dircolors.exe')
+Set-Alias dirname (Join-Path $PSScriptRoot 'bin\dirname.exe')
+Set-Alias du (Join-Path $PSScriptRoot 'bin\du.exe')
+Set-Alias env (Join-Path $PSScriptRoot 'bin\env.exe')
+Set-Alias expand (Join-Path $PSScriptRoot 'bin\expand.exe')
+Set-Alias expr (Join-Path $PSScriptRoot 'bin\expr.exe')
+Set-Alias factor (Join-Path $PSScriptRoot 'bin\factor.exe')
+Set-Alias false (Join-Path $PSScriptRoot 'bin\false.exe')
+Set-Alias fmt (Join-Path $PSScriptRoot 'bin\fmt.exe')
+Set-Alias fold (Join-Path $PSScriptRoot 'bin\fold.exe')
+Set-Alias funzip (Join-Path $PSScriptRoot 'bin\funzip.exe')
+Set-Alias gawk-3.1.6 (Join-Path $PSScriptRoot 'bin\gawk-3.exe')
+Set-Alias gawk (Join-Path $PSScriptRoot 'bin\gawk.exe')
+Set-Alias gdate (Join-Path $PSScriptRoot 'bin\gdate.exe')
+Set-Alias gecho (Join-Path $PSScriptRoot 'bin\gecho.exe')
+Set-Alias ginstall (Join-Path $PSScriptRoot 'bin\ginstall.exe')
+Set-Alias gln (Join-Path $PSScriptRoot 'bin\gln.exe')
+Set-Alias gmkdir (Join-Path $PSScriptRoot 'bin\gmkdir.exe')
+Set-Alias grmdir (Join-Path $PSScriptRoot 'bin\grmdir.exe')
+Set-Alias groups (Join-Path $PSScriptRoot 'bin\groups.exe')
+Set-Alias gsort (Join-Path $PSScriptRoot 'bin\gsort.exe')
+Set-Alias gunzip (Join-Path $PSScriptRoot 'bin\gunzip.exe')
+Set-Alias gzexe (Join-Path $PSScriptRoot 'bin\gzexe.exe')
+Set-Alias gzip (Join-Path $PSScriptRoot 'bin\gzip.exe')
+Set-Alias head (Join-Path $PSScriptRoot 'bin\head.exe')
+Set-Alias hostid (Join-Path $PSScriptRoot 'bin\hostid.exe')
+Set-Alias hostname (Join-Path $PSScriptRoot 'bin\hostname.exe')
+Set-Alias id (Join-Path $PSScriptRoot 'bin\id.exe')
+Set-Alias igawk (Join-Path $PSScriptRoot 'bin\igawk.exe')
+Set-Alias install (Join-Path $PSScriptRoot 'bin\install.exe')
+Set-Alias join (Join-Path $PSScriptRoot 'bin\join.exe')
+Set-Alias less (Join-Path $PSScriptRoot 'bin\less.exe')
+Set-Alias lessecho (Join-Path $PSScriptRoot 'bin\lessecho.exe')
+Set-Alias lessfile (Join-Path $PSScriptRoot 'bin\lessfile.exe')
+Set-Alias lesskey (Join-Path $PSScriptRoot 'bin\lesskey.exe')
+Set-Alias lesspipe (Join-Path $PSScriptRoot 'bin\lesspipe.exe')
+Set-Alias libeay32 (Join-Path $PSScriptRoot 'bin\libeay32.exe')
+Set-Alias libiconv-2 (Join-Path $PSScriptRoot 'bin\libiconv-2.exe')
+Set-Alias libiconv2 (Join-Path $PSScriptRoot 'bin\libiconv2.exe')
+Set-Alias libintl-2 (Join-Path $PSScriptRoot 'bin\libintl-2.exe')
+Set-Alias libintl3 (Join-Path $PSScriptRoot 'bin\libintl3.exe')
+Set-Alias libssl32 (Join-Path $PSScriptRoot 'bin\libssl32.exe')
+Set-Alias link (Join-Path $PSScriptRoot 'bin\link.exe')
+Set-Alias ln (Join-Path $PSScriptRoot 'bin\ln.exe')
+Set-Alias logname (Join-Path $PSScriptRoot 'bin\logname.exe')
+Set-Alias md5sum (Join-Path $PSScriptRoot 'bin\md5sum.exe')
+Set-Alias mkfifo (Join-Path $PSScriptRoot 'bin\mkfifo.exe')
+Set-Alias mknod (Join-Path $PSScriptRoot 'bin\mknod.exe')
+Set-Alias nice (Join-Path $PSScriptRoot 'bin\nice.exe')
+Set-Alias nl (Join-Path $PSScriptRoot 'bin\nl.exe')
+Set-Alias nohup (Join-Path $PSScriptRoot 'bin\nohup.exe')
+Set-Alias od (Join-Path $PSScriptRoot 'bin\od.exe')
+Set-Alias paste (Join-Path $PSScriptRoot 'bin\paste.exe')
+Set-Alias pathchk (Join-Path $PSScriptRoot 'bin\pathchk.exe')
+Set-Alias pcre3 (Join-Path $PSScriptRoot 'bin\pcre3.exe')
+Set-Alias pgawk-3.1.6 (Join-Path $PSScriptRoot 'bin\pgawk-3.exe')
+Set-Alias pgawk (Join-Path $PSScriptRoot 'bin\pgawk.exe')
+Set-Alias pinky (Join-Path $PSScriptRoot 'bin\pinky.exe')
+Set-Alias pr (Join-Path $PSScriptRoot 'bin\pr.exe')
+Set-Alias printenv (Join-Path $PSScriptRoot 'bin\printenv.exe')
+Set-Alias printf (Join-Path $PSScriptRoot 'bin\printf.exe')
+Set-Alias ptx (Join-Path $PSScriptRoot 'bin\ptx.exe')
+Set-Alias readlink (Join-Path $PSScriptRoot 'bin\readlink.exe')
+Set-Alias regex2 (Join-Path $PSScriptRoot 'bin\regex2.exe')
+Set-Alias sed (Join-Path $PSScriptRoot 'bin\sed.exe')
+Set-Alias seq (Join-Path $PSScriptRoot 'bin\seq.exe')
+Set-Alias setuidgid (Join-Path $PSScriptRoot 'bin\setuidgid.exe')
+Set-Alias SFXWiz32 (Join-Path $PSScriptRoot 'bin\SFXWiz32.exe')
+Set-Alias sha1sum (Join-Path $PSScriptRoot 'bin\sha1sum.exe')
+Set-Alias shred (Join-Path $PSScriptRoot 'bin\shred.exe')
+Set-Alias split (Join-Path $PSScriptRoot 'bin\split.exe')
+Set-Alias stat (Join-Path $PSScriptRoot 'bin\stat.exe')
+Set-Alias stty (Join-Path $PSScriptRoot 'bin\stty.exe')
+Set-Alias su (Join-Path $PSScriptRoot 'bin\su.exe')
+Set-Alias sum (Join-Path $PSScriptRoot 'bin\sum.exe')
+Set-Alias sync (Join-Path $PSScriptRoot 'bin\sync.exe')
+Set-Alias tac (Join-Path $PSScriptRoot 'bin\tac.exe')
+Set-Alias tail (Join-Path $PSScriptRoot 'bin\tail.exe')
+Set-Alias tar (Join-Path $PSScriptRoot 'bin\tar.exe')
+Set-Alias test (Join-Path $PSScriptRoot 'bin\test.exe')
+Set-Alias touch (Join-Path $PSScriptRoot 'bin\touch.exe')
+Set-Alias tr (Join-Path $PSScriptRoot 'bin\tr.exe')
+Set-Alias true (Join-Path $PSScriptRoot 'bin\true.exe')
+Set-Alias tsort (Join-Path $PSScriptRoot 'bin\tsort.exe')
+Set-Alias tty (Join-Path $PSScriptRoot 'bin\tty.exe')
+Set-Alias uname (Join-Path $PSScriptRoot 'bin\uname.exe')
+Set-Alias uncompress (Join-Path $PSScriptRoot 'bin\uncompress.exe')
+Set-Alias unexpand (Join-Path $PSScriptRoot 'bin\unexpand.exe')
+Set-Alias uniq (Join-Path $PSScriptRoot 'bin\uniq.exe')
+Set-Alias unlink (Join-Path $PSScriptRoot 'bin\unlink.exe')
+Set-Alias unzip (Join-Path $PSScriptRoot 'bin\unzip.exe')
+Set-Alias unzip32 (Join-Path $PSScriptRoot 'bin\unzip32.exe')
+Set-Alias unzipsfx (Join-Path $PSScriptRoot 'bin\unzipsfx.exe')
+Set-Alias uptime (Join-Path $PSScriptRoot 'bin\uptime.exe')
+Set-Alias users (Join-Path $PSScriptRoot 'bin\users.exe')
+Set-Alias uzexampl (Join-Path $PSScriptRoot 'bin\uzexampl.exe')
+Set-Alias vdir (Join-Path $PSScriptRoot 'bin\vdir.exe')
+Set-Alias wc (Join-Path $PSScriptRoot 'bin\wc.exe')
+Set-Alias who (Join-Path $PSScriptRoot 'bin\who.exe')
+Set-Alias whoami (Join-Path $PSScriptRoot 'bin\whoami.exe')
+Set-Alias yes (Join-Path $PSScriptRoot 'bin\yes.exe')
+Set-Alias zcat (Join-Path $PSScriptRoot 'bin\zcat.exe')
+Set-Alias zcmp (Join-Path $PSScriptRoot 'bin\zcmp.exe')
+Set-Alias zdiff (Join-Path $PSScriptRoot 'bin\zdiff.exe')
+Set-Alias zegrep (Join-Path $PSScriptRoot 'bin\zegrep.exe')
+Set-Alias zfgrep (Join-Path $PSScriptRoot 'bin\zfgrep.exe')
+Set-Alias zforce (Join-Path $PSScriptRoot 'bin\zforce.exe')
+Set-Alias zgrep (Join-Path $PSScriptRoot 'bin\zgrep.exe')
+Set-Alias zip (Join-Path $PSScriptRoot 'bin\zip.exe')
+Set-Alias zip32z64 (Join-Path $PSScriptRoot 'bin\zip32z64.exe')
+Set-Alias zipcloak (Join-Path $PSScriptRoot 'bin\zipcloak.exe')
+Set-Alias zipinfo (Join-Path $PSScriptRoot 'bin\zipinfo.exe')
+Set-Alias zipnote (Join-Path $PSScriptRoot 'bin\zipnote.exe')
+Set-Alias zipsplit (Join-Path $PSScriptRoot 'bin\zipsplit.exe')
+Set-Alias zless (Join-Path $PSScriptRoot 'bin\zless.exe')
+Set-Alias zmore (Join-Path $PSScriptRoot 'bin\zmore.exe')
+Set-Alias znew (Join-Path $PSScriptRoot 'bin\znew.exe')
+
+Export-ModuleMember -Alias `
+    g-cat,
+    g-cp,
+    g-dir,
+    g-echo,
+    g-kill,
+    g-ls,
+    g-mkdir,
+    g-mv,
+    g-pwd,
+    g-rm,
+    g-rmdir,
+    g-sleep,
+    g-sort,
+    g-tee,
+    g-wget,
+
+    awk,
+    basename,
+    bunzip2,
+    bzcat,
+    bzcmp,
+    bzdiff,
+    bzegrep,
+    bzfgrep,
+    bzgrep,
+    bzip2,
+    bzip2,
+    bzip2recover,
+    bzless,
+    bzmore,
+    chgrp,
+    chmod,
+    chown,
+    chroot,
+    cksum,
+    comm,
+    csplit,
+    cut,
+    date,
+    dd,
+    df,
+    dircolors,
+    dirname,
+    du,
+    env,
+    expand,
+    expr,
+    factor,
+    false,
+    fmt,
+    fold,
+    funzip,
+    gawk-3.1.6,
+    gawk,
+    gdate,
+    gecho,
+    ginstall,
+    gln,
+    gmkdir,
+    grmdir,
+    groups,
+    gsort,
+    gunzip,
+    gzexe,
+    gzip,
+    head,
+    hostid,
+    hostname,
+    id,
+    igawk,
+    install,
+    join,
+    less,
+    lessecho,
+    lessfile,
+    lesskey,
+    lesspipe,
+    libeay32,
+    libiconv-2,
+    libiconv2,
+    libintl-2,
+    libintl3,
+    libssl32,
+    link,
+    ln,
+    logname,
+    md5sum,
+    mkfifo,
+    mknod,
+    nice,
+    nl,
+    nohup,
+    od,
+    paste,
+    pathchk,
+    pcre3,
+    pgawk-3.1.6,
+    pgawk,
+    pinky,
+    pr,
+    printenv,
+    printf,
+    ptx,
+    readlink,
+    regex2,
+    sed,
+    seq,
+    setuidgid,
+    SFXWiz32,
+    sha1sum,
+    shred,
+    split,
+    stat,
+    stty,
+    su,
+    sum,
+    sync,
+    tac,
+    tail,
+    tar,
+    test,
+    touch,
+    tr,
+    true,
+    tsort,
+    tty,
+    uname,
+    uncompress,
+    unexpand,
+    uniq,
+    unlink,
+    unzip,
+    unzip32,
+    unzipsfx,
+    uptime,
+    users,
+    uzexampl,
+    vdir,
+    wc,
+    who,
+    whoami,
+    yes,
+    zcat,
+    zcmp,
+    zdiff,
+    zegrep,
+    zfgrep,
+    zforce,
+    zgrep,
+    zip,
+    zip32z64,
+    zipcloak,
+    zipinfo,
+    zipnote,
+    zipsplit,
+    zless,
+    zmore,
+    znew
+
